f089c7f2d4a0b953f47df5960092081ed2379ddf
juliuszCezar <michal.adam.cichy@gmail.com>
2020-01-23 15:01:27 +0100

    fixes multi primary key - important

--- app/Storagerecord.php
+++ app/Storagerecord.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Storagerecord extends Model
 {
@@ -14,5 +15,34 @@ class Storagerecord extends Model
         'product_id'
     ];
 
+    protected $primaryKey = ['date', 'time', 'product_id'];
+    public $incrementing = false;
     public $timestamps = false;
+
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
 }
